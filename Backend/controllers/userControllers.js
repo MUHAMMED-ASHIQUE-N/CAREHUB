a9a97947ea40df4f7c86f45a87d02419a43d1e0e
MUHAMMED-ASHIQUE-N <muhammedashique433@gmail.com>
2025-02-20 10:47:43 +0530

    implemented

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -33,3 +33,27 @@ const userRegister = async (req, res) => {
   }
 };
 
+const userSignin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message || "Internal server error" });
+  }
+};
+
+module.exports = { userRegister, userSignin };
